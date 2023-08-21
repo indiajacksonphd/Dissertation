0a35730c45871f5ad79dcb8c99875ff258ec4a32
India Jackson <110256537+indiajacksonphd@users.noreply.github.com>
2023-08-21 09:27:18 -0400

    Create main.py

---
+++ solar_Survival/main.py
@@ -0,0 +1,9 @@
+from solar_Survival.solarSurvival import welcome_message, data_extraction, results
+
+def main():
+    welcome_message()
+    data_extraction()
+    results()
+
+if __name__ == '__main__':
+    main()
